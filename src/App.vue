a367b471c3522252d99e4205ab0cd34cdf38e184
David Alves <dev.david@aticonsultoria.com>
2016-08-19 17:03:04 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<style>
+
+</style>
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Tarefas></Tarefas>
+  </div>
+</template>
+
+<script>
+import Tarefas from './Tarefas.vue'
+
+export default {
+  compontents: {
+    Tarefas
+  },
+  // data () {
+  //   return {
+  //     msg: 'Hello Vue!'
+  //   }
+  // }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/Tarefas.vue
@@ -0,0 +1,19 @@
+<style>
+	
+</style>
+
+<template>
+	<div id="tarefas">
+		<h1>{{ title }}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Tarefas'
+			}
+		}
+	}
+</script> 
